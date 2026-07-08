7a0e9dfb3ba1045b62167500090fe4edfdc23451
rahulrao1945 <raorahul11123@gmail.com>
2026-07-08 18:03:37 +0530

    ✅ Print Function [python3] — approach1

---
+++ solutions/python3/easy/python-print/solution-approach1.py
@@ -0,0 +1,14 @@
+# ──────────────────────────────────────────────────
+# Problem     Print Function
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-08, 06:03 p.m.
+# ──────────────────────────────────────────────────
+
+n = int(input())
+
+for i in range(1, n + 1):
+    print(i, end="")
