49a401cbc0d6afcba9a1114f177450d1c5b94302
Rahul Yadav <raorahul11123@gmail.com>
2026-07-15 08:02:36 +0530

    ✅ Triangle Quest 2 [python3] — approach1

---
+++ solutions/python3/medium/triangle-quest-2/solution-approach1.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/triangle-quest-2/problem?isFullScreen=true
+# Problem     Triangle Quest 2
+# Difficulty  Medium
+# Subdomain   Math
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-15, 08:02 a.m.
+# ──────────────────────────────────────────────────
+
+
+
+
+for i in range(1, int(input()) + 1):
+    print((10**i // 9) ** 2)
