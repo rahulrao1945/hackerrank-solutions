86b59532b2e4da46f74205470e9a44f13ade0172
Rahul Yadav <raorahul11123@gmail.com>
2026-07-15 08:03:03 +0530

    ✅ Mod Divmod [python3] — approach1

---
+++ solutions/python3/easy/python-mod-divmod/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-mod-divmod/problem?isFullScreen=true
+# Problem     Mod Divmod
+# Difficulty  Easy
+# Subdomain   Math
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-15, 08:03 a.m.
+# ──────────────────────────────────────────────────
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+
+print(a // b)
+print(a % b)
+print(divmod(a, b))
