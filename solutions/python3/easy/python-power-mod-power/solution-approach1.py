989b32292667f7534399478e61c08a5f8e7ece92
Rahul Yadav <raorahul11123@gmail.com>
2026-07-15 08:04:21 +0530

    ✅ Power - Mod Power [python3] — approach1

---
+++ solutions/python3/easy/python-power-mod-power/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+# Problem     Power - Mod Power
+# Difficulty  Easy
+# Subdomain   Math
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-15, 08:04 a.m.
+# ──────────────────────────────────────────────────
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a, b))
+print(pow(a, b, m))
