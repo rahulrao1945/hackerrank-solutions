12ca3be38b32fecfeb384f36febe3a19f044d810
Rahul Yadav <raorahul11123@gmail.com>
2026-07-23 23:25:18 +0530

    ✅ Text Wrap [python3] — approach1

---
+++ solutions/python3/easy/text-wrap/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/text-wrap/problem?isFullScreen=true
+# Problem     Text Wrap
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-07-23, 11:25 p.m.
+# ──────────────────────────────────────────────────
+
+
+
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
+
+
